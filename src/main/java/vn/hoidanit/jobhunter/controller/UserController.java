3812fcf27d4851e1795cf81629ed2d278d908c22
NamPhuong104 <nguyennamphuong104@gmail.com>
2025-07-31 17:07:54 +0700

    update CRUD user

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -1,10 +1,12 @@
 package vn.hoidanit.jobhunter.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.service.UserService;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -19,5 +21,29 @@ public class UserController {
         User res = this.userService.handleCreateUser(user);
 
         return res;
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUser() {
+        List<User> res = this.userService.handleGetAllUser();
+        return res;
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUserById(@PathVariable long id) {
+        return this.userService.handleGetUserById(id);
+    }
+
+    @PutMapping("/users")
+    public User updateUser(@RequestBody User reqData) {
+
+        return this.userService.handleUpdateUser(reqData);
+    }
+
+
+    @DeleteMapping("/users/{id}")
+    public String deleteUser(@PathVariable Long id) {
+        this.userService.handleDeleteUser(id);
+        return "success";
     }
 }
--- src/main/java/vn/hoidanit/jobhunter/service/UserService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/UserService.java
@@ -1,8 +1,13 @@
 package vn.hoidanit.jobhunter.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.repository.UserRepository;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,5 +19,38 @@ public class UserService {
 
     public User handleCreateUser(User user) {
         return this.userRepository.save(user);
+    }
+
+    public List<User> handleGetAllUser() {
+        return this.userRepository.findAll();
+    }
+
+    public User handleGetUserById(long id
+    ) {
+        Optional<User> optionalUser = this.userRepository.findById(id);
+        if (optionalUser.isPresent()) {
+            return optionalUser.get();
+        }
+        return null;
+
+    }
+
+    public User handleUpdateUser(User reqData) {
+        User currentUser = this.handleGetUserById(reqData.getId());
+
+        if (currentUser != null) {
+            currentUser.setEmail(reqData.getEmail());
+            currentUser.setName(reqData.getName());
+            currentUser.setPassword(reqData.getPassword());
+
+            currentUser = this.userRepository.save(currentUser);
+
+        }
+        return currentUser;
+    }
+
+    public void handleDeleteUser(long id) {
+
+        this.userRepository.deleteById(id);
     }
 }
