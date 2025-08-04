bad517af8b16c6a3a9b725bdcaefb825d6aba538
NamPhuong104 <nguyennamphuong104@gmail.com>
2025-08-04 16:26:32 +0700

    create jwt and return format response

--- src/main/java/vn/hoidanit/jobhunter/controller/AuthController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/AuthController.java
@@ -9,18 +9,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import vn.hoidanit.jobhunter.domain.LoginDTO;
+import vn.hoidanit.jobhunter.domain.RestLoginDTO;
+import vn.hoidanit.jobhunter.util.SecurityUtil;
 
 @RestController
 public class AuthController {
 
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final SecurityUtil securityUtil;
 
-    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder) {
+    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder, SecurityUtil securityUtil) {
         this.authenticationManagerBuilder = authenticationManagerBuilder;
+        this.securityUtil = securityUtil;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginDTO> login(@Valid @RequestBody LoginDTO loginDto) {
+    public ResponseEntity<RestLoginDTO> login(@Valid @RequestBody LoginDTO loginDto) {
 
         //Nạp input gồm username/password vào Security
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
@@ -28,7 +32,11 @@ public class AuthController {
         //xác thực người dùng => cần viết hàm loadUserByUsername
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
 
+        String access_token = this.securityUtil.createToken(authentication);
+        RestLoginDTO res = new RestLoginDTO();
 
-        return ResponseEntity.ok().body(loginDto);
+        res.setAccessToken(access_token);
+
+        return ResponseEntity.ok().body(res);
     }
 }
